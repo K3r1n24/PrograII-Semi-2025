009a8faa8ff5bbd31de13d358b3fc5f4346a82a7
K3r1n24 <kerindepaz11@gmail.com>
2025-02-04 10:04:11 -0600

    Calculadora con opciones de suma hasta factorial

--- app/src/main/java/com/ugb/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/ugb/myfirstapplication/MainActivity.java
@@ -1,24 +1,21 @@
 package com.ugb.myfirstapplication;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
     RadioGroup rgb;
     RadioButton opt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +49,43 @@ public class MainActivity extends AppCompatActivity {
                 if (opt.isChecked()) {
                     respuesta = num1 / num2;
                 }
+                opt = findViewById(R.id.optExponente);
+                if (opt.isChecked()) {
+                    respuesta = Math.pow(num1, num2);
+
+                }
+                opt = findViewById(R.id.optPorcentaje);
+                if (opt.isChecked()) {
+                    respuesta = (num1 * num2) / 100;
+                }
+                opt = findViewById(R.id.optRaiz);
+                if (opt.isChecked()) {
+                    respuesta = num1 >= 0 ? Math.sqrt(num1) : Double.NaN;
+                }
+                opt = findViewById(R.id.optFactorial);
+                if (opt.isChecked()) {
+                    if (num1 < 0 || num1 != (int) num1) {
+                        tempVal = findViewById(R.id.lblRespuesta);
+                        tempVal.setText("Error: Factorial solo para enteros positivos.");
+                        return;
+                    }
+                    respuesta = calcularFactorial((int) num1);
+                }
+
                 tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+                tempVal.setText("Respuesta: " + respuesta);
             }
         });
     }
+
+    private long calcularFactorial(int num) {
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
+
+
+
